6a9b41717688abde0409cc82944e2f219c8c9774
xiaozhang199 <75928805+xiaozhang199@users.noreply.github.com>
2021-10-31 16:29:32 +0800

    Create 1009.cpp

---
+++ 1009.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    stack <string> v;
+    string s;
+    while(cin >> s){
+        v.push(s);
+    }
+    cout << v.top();
+    v.pop();
+
+    while(!v.empty()){
+        cout << " " << v.top();
+        v.pop();
+    }
+    return 0;
+}
